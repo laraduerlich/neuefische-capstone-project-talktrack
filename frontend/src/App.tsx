a7f0be3c0edaa34ed9a51154cedcd32e0b48e9fc
laraduerlich <lara.duerlich@t-online.com>
2025-01-27 18:11:19 +0100

    feat: add pages and routes in the app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from "./component/Header.tsx";
+import {Route, Routes} from "react-router-dom";
+import LinkPage from "./page/LinkPage.tsx";
+import LandingPage from "./page/LandingPage.tsx";
+import DashboardPage from "./page/DashboardPage.tsx";
+import SummaryPage from "./page/SummaryPage.tsx";
+import Footer from "./component/Footer.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div>
+            <Header />
+            <div>
+                <Routes>
+                    <Route path={"/"} element={<LandingPage />} />
+                    <Route path={"/home"} element={<DashboardPage />} />
+                    <Route path={"/newsummary"} element={<LinkPage />} />
+                    <Route path={"/summary/:id"} element={<SummaryPage />} />
+                </Routes>
+            </div>
+            <Footer />
+        </div>
     </>
   )
 }
---
+++ frontend/src/component/Footer.tsx
@@ -0,0 +1,11 @@
+
+export default function Footer() {
+
+    return (
+        <>
+            <footer>
+                <p>This is the footer</p>
+            </footer>
+        </>
+    )
+}
