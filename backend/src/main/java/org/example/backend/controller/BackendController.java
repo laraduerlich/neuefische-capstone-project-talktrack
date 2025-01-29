fa53c839bd0e2cc1797efaa4da9d285f1dc4cc69
laraduerlich <lara.duerlich@t-online.com>
2025-01-29 22:12:12 +0100

    fix: remove unused imports

--- backend/src/main/java/org/example/backend/controller/BackendController.java
+++ backend/src/main/java/org/example/backend/controller/BackendController.java
@@ -7,11 +7,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Mono;
 
-import java.io.File;
 import java.io.IOException;
 
 @RestController
