f79004b51e2bdbd969f799acf47f63d361b224c7
laraduerlich <lara.duerlich@t-online.com>
2025-01-30 10:13:24 +0100

    fix: Sonar issues

--- backend/src/main/java/org/example/backend/controller/BackendController.java
+++ backend/src/main/java/org/example/backend/controller/BackendController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Mono;
-
 import java.io.IOException;
 
 @RestController
--- backend/src/main/java/org/example/backend/exception/UploadFailedException.java
+++
@@ -1,7 +0,0 @@
-package org.example.backend.exception;
-
-public class UploadFailedException extends Exception {
-    public UploadFailedException(String message) {
-        super(message);
-    }
-}
