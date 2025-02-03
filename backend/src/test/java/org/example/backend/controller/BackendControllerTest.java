097583ad933b03d17ef6e5362359c68c536b2134
laraduerlich <lara.duerlich@t-online.com>
2025-02-03 14:51:02 +0100

    fix: fixed tests for BackendController

--- backend/src/test/java/org/example/backend/controller/BackendControllerTest.java
+++ backend/src/test/java/org/example/backend/controller/BackendControllerTest.java
@@ -1,9 +1,16 @@
 package org.example.backend.controller;
 
+import org.example.backend.model.Summary;
+import org.example.backend.model.assemblyai.AssemblyAiResponse;
+import org.example.backend.model.assemblyai.FileUploadRequest;
+import org.example.backend.service.AssemblyAiService;
+import org.example.backend.service.SummaryService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -11,7 +18,10 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -28,9 +38,15 @@ class BackendControllerTest {
     @Autowired
     BackendController backendController;
 
+    @MockBean
+    private AssemblyAiService assemblyAiService;
+
+    @MockBean
+    private SummaryService service;
+
     @DynamicPropertySource
     static void registerAssemblyAiProperties(DynamicPropertyRegistry registry) {
-        registry.add("assemblyai.api.key", () -> "test-assembly");
+        registry.add("app.assemblyai.api.key", () -> "test-assembly");
     }
 
     @Test
@@ -42,6 +58,13 @@ class BackendControllerTest {
                 "audio/m4a",                // MIME-Typ
                 "This is a test file".getBytes()  // Dateiinhalt als Byte-Array
         );
+
+        AssemblyAiResponse mockResponse = new AssemblyAiResponse("mock-file-id");
+        Summary mockSummary = new Summary("1","title", "Kurzfassung");
+
+        when(assemblyAiService.uploadFile(any(FileUploadRequest.class))).thenReturn(mockResponse);
+        when(assemblyAiService.transcriptFile(mockResponse)).thenReturn(Optional.of("Test Transkript"));
+        when(service.createSummary("Test Transkript")).thenReturn(mockSummary);
 
         // WHEN & THEN
         mockMvc.perform(multipart("/api/upload")
--- backend/src/test/java/org/example/backend/service/SummaryServiceTest.java
+++ backend/src/test/java/org/example/backend/service/SummaryServiceTest.java
@@ -2,7 +2,6 @@ package org.example.backend.service;
 
 import org.example.backend.model.Summary;
 import org.example.backend.repo.SummaryRepo;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
