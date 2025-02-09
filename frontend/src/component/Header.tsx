b0a0c9599e5ea1cdbfb9faa64ea12b67ea94ae91
laraduerlich <lara.duerlich@t-online.com>
2025-02-09 12:47:49 +0100

    feat: add NavBar for better Navigation

--- frontend/src/component/Header.tsx
+++ frontend/src/component/Header.tsx
@@ -1,10 +1,53 @@
 
 export default function Header() {
 
+    const navLinks = [
+        { name: "Dashboard", path: "/home" },
+        { name: "Neu", path: "/summary/new"}
+    ];
+
     return (
         <>
-            <header className="bg-gray-800 p-4 text-white">
-                <h1 className="text-center text-2xl">TalkTrack</h1>
+            <header className="backdrop-blur-md bg-white/30 p-6 text-gray-800
+            {/*[box-shadow:0px_6px_12px_rgba(244,67,54,0.2)]*/}">
+                <div className="container flex justify-between h-16 mx-auto">
+                    <a
+                        rel="noopener noreferrer"
+                        href="/"
+                        aria-label="Back to homepage"
+                        className="flex items-center"
+                    >
+                        {/* App Logo */}
+                        <img
+                            src="/app-logo.png"
+                            alt="TalkTrack"
+                            className="w-16 h-16 object-contain"
+                        />
+                        {/* App Name */}
+                        <img
+                            src="/app-name.png"
+                            alt="App Name"
+                            className="ml-4 hidden md:block h-24 object-contain"
+                        />
+                    </a>
+
+
+                    {/* Navigation */}
+                    <ul className="items-stretch space-x-3 md:flex">
+                        {navLinks.map((link) => (
+                            <li key={link.name} className="flex">
+                                <a
+                                    rel="noopener noreferrer"
+                                    href={link.path}
+                                    className="flex items-center px-4 -mb-1 border-b-2 border-transparent hover:border-gray-800"
+                                >
+                                    {link.name}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+
+                </div>
             </header>
         </>
     )
